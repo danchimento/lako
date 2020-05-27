3497b2f6a55ca1c6708ac38eb0a061ac1f10d870
Dan Chimento <danchimento@live.com>
2020-05-27 15:45:06 -0400

    Simplify example code

--- example/game.js
+++ example/game.js
@@ -1,13 +1,4 @@
-var box1 = Game.addText();
-box1.text = "LETS GO!!"
-box1.width = 500;
-box1.textColor = "white"
-box1.fontSize = "100"
-
-Game.color = "blue"
-
+var box1 = Game.addRectangle();
+box1.width = 100;
+box1.height = 200;
 box1.backgroundColor = "red";
-
-box1.click = function() {
-    box1.left = box1.left + 10;
-};
